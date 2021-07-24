a4ba6f3e34fc5c2d7ea4975be21b40f9a08892f0
Ram123807 <71751956+Ram123807@users.noreply.github.com>
2021-07-24 19:19:11 +0530

    Add files via upload

---
+++ src/hotelrecommendationsystem/HotelRecSystemClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelrecommendationsystem;
+
+/**
+ *
+ * @author Vijay Raj Singh
+ */
+public class HotelRecSystemClass {
+    
+    public static void main(String[] args) {
+       
+        HLoginPage hp = new HLoginPage();
+        hp.setVisible(true);
+       
+    }
+}
